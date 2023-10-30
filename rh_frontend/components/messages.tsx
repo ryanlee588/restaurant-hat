1fc1dba014d2f583f98d8b98415b109de42c91f6
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-10-30 16:31:42 +0800

    feat: changed color of message box

--- rh_frontend/components/messages.tsx
+++ rh_frontend/components/messages.tsx
@@ -1,28 +1,20 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-// import { useToast } from "@/components/ui/use-toast";
 
 export default function Messages() {
   const searchParams = useSearchParams();
   const error = searchParams.get("error");
   const message = searchParams.get("message");
-  // const { toast } = useToast();
-  // if (error) {
-  //   toast({ title: "Error", description: error });
-  // }
-  // if (message) {
-  //   toast({ title: "Message", description: message });
-  // }
   return (
     <>
       {error && (
-        <p className="mt-4 p-4 bg-foreground/10 text-white text-center">
+        <p className="mt-4 p-4 border border-slate-500 bg-foreground/10 text-white text-center">
           {error}
         </p>
       )}
       {message && (
-        <p className="mt-4 p-4 bg-foreground/10 text-white text-center">
+        <p className="mt-4 p-4 border border-slate-500 bg-foreground/10 text-white text-center">
           {message}
         </p>
       )}
