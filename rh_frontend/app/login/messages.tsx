5fb6c937c71e702f5baa3008a98f1706aa0696b5
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-10-30 15:35:03 +0800

    feat: changed log in error to toasts

--- rh_frontend/app/login/messages.tsx
+++ rh_frontend/app/login/messages.tsx
@@ -1,23 +1,18 @@
-'use client'
+"use client";
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Messages() {
-  const searchParams = useSearchParams()
-  const error = searchParams.get('error')
-  const message = searchParams.get('message')
-  return (
-    <>
-      {error && (
-        <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-          {error}
-        </p>
-      )}
-      {message && (
-        <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-          {message}
-        </p>
-      )}
-    </>
-  )
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+  const message = searchParams.get("message");
+  const { toast } = useToast();
+  if (error) {
+    toast({ title: "Error", description: error });
+  }
+  if (message) {
+    toast({ title: "Message", description: message });
+  }
+  return <></>;
 }
