e3f812f31156bc09a28a3eadb6eea3c1a04d55fb
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-10-30 16:19:59 +0800

    fix: switched back to message box

--- rh_frontend/components/messages.tsx
+++ rh_frontend/components/messages.tsx
@@ -1,18 +1,31 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useToast } from "@/components/ui/use-toast";
+// import { useToast } from "@/components/ui/use-toast";
 
 export default function Messages() {
   const searchParams = useSearchParams();
   const error = searchParams.get("error");
   const message = searchParams.get("message");
-  const { toast } = useToast();
-  if (error) {
-    toast({ title: "Error", description: error });
-  }
-  if (message) {
-    toast({ title: "Message", description: message });
-  }
-  return <></>;
+  // const { toast } = useToast();
+  // if (error) {
+  //   toast({ title: "Error", description: error });
+  // }
+  // if (message) {
+  //   toast({ title: "Message", description: message });
+  // }
+  return (
+    <>
+      {error && (
+        <p className="mt-4 p-4 bg-gray-700 text-foreground text-center">
+          {error}
+        </p>
+      )}
+      {message && (
+        <p className="mt-4 p-4 bg-gray-700 text-foreground text-center">
+          {message}
+        </p>
+      )}
+    </>
+  );
 }
